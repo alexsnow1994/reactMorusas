a0979d341c6cf582782efbfcb501280d56198893
Alejandro Estrada <ing.estrada.rocha@gmail.com>
2022-01-23 15:07:59 -0600

    Agregando componente Header

---
+++ src/components/MorusasHeader.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function MorusasHeader() {
+  return <>
+    <div className='container-fluid navbar-bar'>
+        .<div className='row'>
+            <div className="col-4">
+            <h1>Morusas de Ciencia <img href="" src='*'></img></h1>
+            </div>
+            <div className='col-4'>
+                <p>Quienes somos?</p>
+                </div>
+                <div className='col-4'>
+                    <p>Redes sociales</p>
+                </div>  
+        </div>
+        
+    </div>
+  </>;
+}
