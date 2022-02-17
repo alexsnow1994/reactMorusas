9b167d48e0c654c0252cc009ec43e8db7751b0b5
Alejandro EStrada <ing.estrada.rocha@gmail.com>
2022-02-16 22:35:56 -0600

    preparando barra de navegacion

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 
-// import './../styles/App.css';
+import './styles/App.css'
 import { BrowserRouter, Routes , Route } from 'react-router-dom';
 import Layout from './components/Layout/index';
+import Home from './components/Home';
+import MainState from './context/mainContext/MainState';
 
 function App() {
   return (
     <>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout/>}/>
+        <Route path="/" element={<Layout/>}>
+          <Route path="/" element={<Home />}/>
+          <Route path='/Main' element={<MainState/>}/>
+        </Route>
       </Routes>
     </BrowserRouter>
     </>
