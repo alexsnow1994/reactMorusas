764b9f2763f69d92b9f0f4f5adfcbd361f41ec04
Mike Nieva <m@nieva.team>
2022-02-26 10:59:15 -0600

    Ajustes de cierre de módulo

--- src/context/bookContext/BookReducer.js
+++ src/context/bookContext/BookReducer.js
@@ -1,11 +1,17 @@
-export default  (globalState, action) => {
+const reducer = (globalState, action) => {
+
     switch(action.type){
+
         case "GET_ALL_BOOKS":
             return {
                 ...globalState,
                 books: action.payload
             }
+
         default:
             return globalState
+            
     }
 };
+
+export default reducer
