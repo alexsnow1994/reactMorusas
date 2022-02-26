a900845b99c653c6ab352b19826a387c23ec546f
Alejandro Estrada <ing.estrada.rocha@gmail.com>
2022-02-25 21:05:11 -0600

    componiendo

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -11,7 +11,7 @@ export default function Layout() {
       <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
       
         <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-          <span className="sr-only">Open main menu</span>
+        <span className="sr-only">Open main menu</span>
          
           <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -23,14 +23,14 @@ export default function Layout() {
         </button>
       </div>
       <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-        <div className="flex-shrink-0 flex items-center ">
+        <div className="flex-shrink-0 flex items-center font-mono ">
           <img className="block  h-12 w-auto rounded-full Logo" src="http://drive.google.com/uc?export=view&id=107wsbiRk_b242azkeAcwg3yQPGbChaw-" alt="Morusas"/>
           <a>MORUSAS DE CIENCIAS</a>
           {/* <img className="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow"/> */}
         </div>
         <div className="hidden sm:block sm:ml-6">
           <div className="flex text-center space-x-4">
-           <Link to='/bookform' type='button' className='bg-fuchsia-300 rounded-lg'>
+           <Link to='/bookform' type='button' className='bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 text-white rounded-lg'>
            <a  aria-current="page">Tu Libro</a>
            </Link>
             
